d1f9cbc2ea9bd5a878508eba75cd7df02bfb8bec
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-07-14 12:10:09 -0500

    Scope

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,6 @@
+import MediaPlayer from './mediaplayer.js'
 const video = document.querySelector('video')
-      const button = document.querySelector('button')
-      
-      function MediaPlayer(config){
-        this.media = config.el
-      }
-      
-      MediaPlayer.prototype.play = function(){
-        (this.media.paused)
-        ?this.media.play()
-        :this.media.pause()
-      }
-      
-      const player = new MediaPlayer({el:video})
-      
-      button.onclick=()=>player.play()
+const button = document.querySelector('button')
+
+const player = new MediaPlayer({ el: video })
+button.onclick = () => player.play()
---
+++ assets/mediaplayer.js
@@ -0,0 +1,10 @@
+function MediaPlayer(config){
+    this.media = config.el
+  }
+  
+  MediaPlayer.prototype.play = function(){
+    (this.media.paused)
+    ?this.media.play()
+    :this.media.pause()
+  }
+  export default MediaPlayer
