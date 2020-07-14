de68ce09da6cd5c5c7a47f9e57992dec93845bac
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-07-14 13:20:35 -0500

    AutoPlay and mute

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,11 @@
 import MediaPlayer from './mediaplayer.js'
+import Autoplay from './plugins/autoplay.js'
+
+
 const video = document.querySelector('video')
 const button = document.querySelector('button')
+const bmute = document.getElementById('b_mute')
 
-const player = new MediaPlayer({ el: video })
-button.onclick = () => player.play()
+const player = new MediaPlayer({ el: video, plugins:[new Autoplay()]})
+button.onclick = () => player.togglePlay()
+bmute.onclick = () => player.toggleMute()
--- assets/mediaplayer.js
+++ assets/mediaplayer.js
@@ -1,10 +1,22 @@
 function MediaPlayer(config){
     this.media = config.el
+    this.plugins = config.plugins || []
+    this.initPlugins() 
+  }
+  MediaPlayer.prototype.initPlugins=function(){
+    this.plugins.forEach(plugin =>{
+      plugin.run(this)
+    })
   }
   
-  MediaPlayer.prototype.play = function(){
+  MediaPlayer.prototype.togglePlay = function(){
     (this.media.paused)
     ?this.media.play()
     :this.media.pause()
+  }
+  MediaPlayer.prototype.toggleMute = function(){
+    (!this.media.muted)
+    ?this.media.muted=true
+    :this.media.muted=false
   }
   export default MediaPlayer
