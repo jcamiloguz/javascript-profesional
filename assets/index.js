54c3d437340b09a96584635e9501594121239130
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-07-15 09:41:43 -0500

    this class

--- assets/index.js
+++ assets/index.js
@@ -6,6 +6,8 @@ const video = document.querySelector('video')
 const button = document.querySelector('button')
 const bmute = document.getElementById('b_mute')
 
-const player = new MediaPlayer({ el: video, plugins:[new Autoplay()]})
+const player = new MediaPlayer({ el: video, plugins:[
+//    new Autoplay()
+]})
 button.onclick = () => player.togglePlay()
 bmute.onclick = () => player.toggleMute()
